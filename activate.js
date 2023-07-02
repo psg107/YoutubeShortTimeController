4c8c3451e0b0d9264d04d7ae5c4d1c44222efd91
psg107 <psg107@naver.com>
2023-07-03 00:13:32 +0900

    버그 수정

--- activate.js
+++ activate.js
@@ -8,47 +8,49 @@
         const videoContainer = document.querySelector('#shorts-player > div.html5-video-container');
         const video = document.querySelector("#shorts-player > div.html5-video-container > video");
         
-        //create new progressBar
-        if (document.querySelector(`#${CUSTOM_METER_ID}`) == null) {
-            console.log('[YoutubeShortTimeController] create new progressBar');
-
-            let controller = document.createElement('meter');
-            controller.id = CUSTOM_METER_ID;
-            controller.style.width = '100%';
-            controller.style.height = '20px';
-            controller.style.position = 'absolute';
-            controller.style.bottom = '-5px';
-            controller.style.cursor = 'pointer'
-            controller.min = 0;
-    
-            videoContainer.style.height = '100%';
-            videoContainer.appendChild(controller);
-    
-            //events
-            video.addEventListener('timeupdate', (e) => {
-                var duration = video.duration;
-                var currentTime = video.currentTime;
-    
-                controller.max = isNaN(duration) ? 0 : duration;
-                controller.value = isNaN(duration) ? 0 : currentTime;
-            });
-            controller.addEventListener('mouseup', (e) => {
-                var rect = e.target.getBoundingClientRect();
-    
-                var x = e.clientX - rect.left;
-                var width = rect.width;
-    
-                var percent = x / width;
-                var maxTime = controller.max;
-    
-                var time = maxTime * percent;
-    
-                video.currentTime = time;
-            });
-            controller.addEventListener('click', (e) => {
-                e.stopPropagation();
-            });
+        //remove
+        var progressBar = document.querySelector(`#${CUSTOM_METER_ID}`);
+        if (progressBar != null) {
+            progressBar.remove();
         }
+
+        //create new progressBar
+        let controller = document.createElement('meter');
+        controller.id = CUSTOM_METER_ID;
+        controller.style.width = '100%';
+        controller.style.height = '20px';
+        controller.style.position = 'absolute';
+        controller.style.bottom = '-5px';
+        controller.style.cursor = 'pointer'
+        controller.min = 0;
+
+        videoContainer.style.height = '100%';
+        videoContainer.appendChild(controller);
+
+        //events
+        video.addEventListener('timeupdate', (e) => {
+            var duration = video.duration;
+            var currentTime = video.currentTime;
+
+            controller.max = isNaN(duration) ? 0 : duration;
+            controller.value = isNaN(duration) ? 0 : currentTime;
+        });
+        controller.addEventListener('mouseup', (e) => {
+            var rect = e.target.getBoundingClientRect();
+
+            var x = e.clientX - rect.left;
+            var width = rect.width;
+
+            var percent = x / width;
+            var maxTime = controller.max;
+
+            var time = maxTime * percent;
+
+            video.currentTime = time;
+        });
+        controller.addEventListener('click', (e) => {
+            e.stopPropagation();
+        });
     });
 })();
 
